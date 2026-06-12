b14d676656e9888a50a54ef905ca344f30bb44ce
Bhumika Ramawat <aakashramawat@Aakashs-MacBook-Pro.local>
2026-06-12 17:46:28 +0530

    Remove unused Skills import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
-import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
